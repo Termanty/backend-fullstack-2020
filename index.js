064052d1c8417cf0989062e798780ef1f8de35d5
termanty <termanty@gmail.com>
2020-03-31 20:28:23 +0300

    ready for deploy to Heroku???

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('POST-body', (req, res) => {
   return req.method === "POST" ?
@@ -8,6 +9,7 @@ morgan.token('POST-body', (req, res) => {
 })
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :POST-body'))
 
@@ -88,7 +90,7 @@ app.post('/api/persons', (req, res) => {
   
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT)
 })
