fa837d068ac7aa9101d3b091fc1f55f99aed6794
Tero Mäntylä <termanty@gmail.com>
2020-04-03 10:40:09 +0300

    new fron build

---
+++ build/precache-manifest.5d19ef943caf8c8b469e5db3ddaea849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8aa444a22226975037d76ac35420a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee6706e0b16be683bf2",
+    "url": "/static/css/main.2f306ed3.chunk.css"
+  },
+  {
+    "revision": "8b6ee72a1b35c5d5fb35",
+    "url": "/static/js/2.ee1de2d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ee1de2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee6706e0b16be683bf2",
+    "url": "/static/js/main.94577245.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
