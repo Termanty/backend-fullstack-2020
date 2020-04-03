2bd3783dbd0e4db74ac49fe39a28e6df330ec991
Tero Mäntylä <termanty@gmail.com>
2020-04-03 10:29:20 +0300

    Front-end build added

---
+++ build/precache-manifest.17d731bec62da2ebd8e3a5cfc044bbc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7baaf1be4fc8c121a4e3a648199644ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63837c3af0665d3c70ff",
+    "url": "/static/css/main.2f306ed3.chunk.css"
+  },
+  {
+    "revision": "8b6ee72a1b35c5d5fb35",
+    "url": "/static/js/2.ee1de2d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ee1de2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63837c3af0665d3c70ff",
+    "url": "/static/js/main.d8b9a501.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
